2bb92ec52d07418a64531128363ac9bb1550087e
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-05-07 20:55:01 -0300

    first commit

---
+++ src/main/java/com/syscall/SyscallApplication.java
@@ -0,0 +1,12 @@
+package com.syscall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyscallApplication.class, args);
+	}
+}
