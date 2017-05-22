250e5034e84aa5167881649db614a2e1a7962002
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-05-21 23:48:29 -0300

    fea: add alguns testes

--- src/test/java/com/syscall/navegacao/NavegacaoTests.java
+++ src/test/java/com/syscall/navegacao/NavegacaoTests.java
@@ -1,14 +1,20 @@
 package com.syscall.navegacao;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.with;
+
 import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.fluentlenium.core.events.ElementListener;
 import org.fluentlenium.core.hook.wait.Wait;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.TimeUnit;
 
 
 @RunWith(SpringRunner.class)
@@ -16,9 +22,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @Wait
 public class NavegacaoTests  extends FluentTest {
 
-	@Value("${local.server.port}") int port;
+	@Value("${local.server.port}")
+	int port;
+
 	String getUrl(String file) {
-		return "http://localhost:"+ port+ "/prototypes/"+ file +".html";
+		return "http://localhost:" + port + "/prototypes/" + file + ".html";
 	}
 
 	@Test
@@ -33,7 +41,7 @@ public class NavegacaoTests  extends FluentTest {
 
 
 	@Test
-	public void validaNavegacaoCadastro() {
+	public void validaNavegacaoCadastroOperadores() {
 
 		goTo(getUrl("usuarios"));
 		assertThat(window().title()).contains("SYSCALL | Operadores");
@@ -43,11 +51,60 @@ public class NavegacaoTests  extends FluentTest {
 	}
 
 	@Test
-	public void validaNavegacaoMesagemCadastro() {
+	public void validaNavegacaoMensagemCadastroOperadores() {
 
 		goTo(getUrl("usuario_cadastrar"));
 		assertThat(window().title()).contains("SYSCALL | Cadastrar Operador");
+
 		$("#salvar").submit();
+		assertThat($("#sucesso").text()).contains("Sucesso. Dados salvos com successo !");
+
+	}
+
+	@Test
+	public void validaNavegacaoMensagemErroCadastroOperadores() {
+		goTo(getUrl("usuario_cadastrar_erro"));
+		assertThat(window().title()).contains("SYSCALL | Cadastrar Operador");
+
+
+		$("#salvar").submit();
+
+		assertThat($("#erroCadastro").text()).contains("Erro. Erro a o salvar os dados !");
+	}
+
+	@Test
+	public void validaNavegacaoBuscaOperadores() {
+		goTo(getUrl("usuarios"));
+
+		$("#email").fill().with("teste@gmail.com");
+
+		$("#buscar").submit();
+
+		assertThat($("#busca_sem_resultado").text()).contains("Alerta. Nenhum registro encontrado para essa busca !");
+	}
+
+	@Test
+	public void validaNavegacaoEditarOperador() {
+		goTo(getUrl("usuarios"));
+
+		$("#editar_operador").click();
+
+		assertThat(window().title()).contains("SYSCALL | Editar Operador");
+
+		assertThat($("#nome").value().isEmpty()).isFalse();
+	}
+
+	@Test
+	public void validaNavegacaoExcluirOperador() {
+		goTo(getUrl("usuarios"));
+		events().beforeClickOn(new ElementListener() {
+			@Override
+			public void on(FluentWebElement element, WebDriver driver) {
+			 String style = 	el("#modalDelete").attribute("style");
+				assertThat(style).hasToString("display: none;");
+			}
+		});
+        $("#deletar_operador").click();
 
 	}
 
