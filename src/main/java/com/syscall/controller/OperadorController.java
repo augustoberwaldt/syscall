3fabf100ea28fbead78379b84da6af32a188adea
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-05-29 00:42:11 -0300

    fix: add listagem de operadores

---
+++ src/main/java/com/syscall/controller/OperadorController.java
@@ -0,0 +1,17 @@
+package com.syscall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/operador")
+public class OperadorController {
+
+    @GetMapping("/listagem")
+    public String listagem() {
+        return "operador/listagem";
+    }
+
+
+}
---
+++ src/main/java/com/syscall/service/OperadorService.java
@@ -0,0 +1,7 @@
+package com.syscall.service;
+
+/**
+ * Created by harley on 28/05/2017.
+ */
+public class OperadorService {
+}
