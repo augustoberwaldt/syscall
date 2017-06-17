1420016c3318c658871dfbf9e293eee20e8e3133
augustoberwaldt <augusto.berwaldt@gmail.com>
2017-06-17 15:54:16 -0300

    FIX:ajusta template

--- src/main/java/com/syscall/config/MVCConf.java
+++ src/main/java/com/syscall/config/MVCConf.java
@@ -2,13 +2,21 @@ package com.syscall.config;
 import java.util.Locale;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.Ordered;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -16,7 +24,8 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 /**
  * Created by harley on 26/05/2017.
  */
-
+@ComponentScan
+@EnableAutoConfiguration
 @Configuration
 public class MVCConf extends WebMvcConfigurerAdapter {
 
@@ -28,12 +37,23 @@ public class MVCConf extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.PRC);
-        return slr;
+    public CookieLocaleResolver  localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.PRC);
+        localeResolver.setCookieName("lang");
+        localeResolver.setCookieMaxAge(3600);
+        return localeResolver;
+
     }
-    
+
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/i18n/messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
     
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
@@ -42,8 +62,9 @@ public class MVCConf extends WebMvcConfigurerAdapter {
         return lci;
     }
 
-    
-    
+
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
--- src/main/java/com/syscall/controller/HomeController.java
+++ src/main/java/com/syscall/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
