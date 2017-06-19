c0172fbdd4fe4257457d127ba9a880b58454877a
augusto.moovin <augusto.marlon@moovin.com.br>
2017-06-19 18:51:59 -0300

    REF:altera pacote

--- src/main/java/com/syscall/config/Messages.java
+++ src/main/java/com/syscall/config/Messages.java
@@ -1,4 +1,4 @@
-package com.syscall.domain;
+package com.syscall.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.MessageSourceAccessor;
